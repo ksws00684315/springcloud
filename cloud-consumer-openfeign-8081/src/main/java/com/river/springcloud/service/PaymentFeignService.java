c76b9cdb6ac2f319a8c7daf241c35e4c6d9b53cc
river <247806295@qq.com>
2020-10-17 12:13:57 +0800

    hystrix添加默认降级处理方法

--- cloud-consumer-openfeign-8081/src/main/java/com/river/springcloud/service/PaymentFeignService.java
+++ cloud-consumer-openfeign-8081/src/main/java/com/river/springcloud/service/PaymentFeignService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(value = "CLOUD-HYSTRIX-PAYMENT-SERVICE")
+@FeignClient(value = "CLOUD-HYSTRIX-PAYMENT-SERVICE",fallback = PaymentFeiginFallBackService.class)
 public interface PaymentFeignService {
     /*@GetMapping("/payment/hystrix/timeout/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id);*/
