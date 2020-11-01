589d658de4bb73bca3b2f4acb43e73c781ce4a5f
ksws00684315 <247806295@qq.com>
2020-11-01 23:21:39 +0800

    Nacos服务注册中心，服务配置中心搭建

---
+++ cloudalibaba-consumer-nacos-order8083/src/main/java/com/river/cloudalibaba/OrderNacosMain8083.java
@@ -0,0 +1,13 @@
+package com.river.cloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderNacosMain8083 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain8083.class,args);
+    }
+}
